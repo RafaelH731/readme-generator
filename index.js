90a6af56262fb94a253f9ee53ca620a69b03b5da
Rafael Herrera <rafaelherrera731@gmail.com>
2022-04-20 21:54:14 -0700

    added comments to follow

--- index.js
+++ index.js
@@ -4,10 +4,20 @@
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    //get file name and template file
+    //using fs to read and write file and create the readme.md
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    //code goes in here, using the questions var I can use the questions in the inquirer to the user 
+    //pass the answers into generateMarkDown.js
+    //return the template string from generatemarkdown.js
+    //pass template sting to writetofile(Line7) function
 
-// Function call to initialize app
+}
+
+// Function call to initialize app 
 init();
+
